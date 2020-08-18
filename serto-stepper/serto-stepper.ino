282dca82e9f655bae73b77dd6b7c4ff21849ee06
jose107-crebots <57700967+jose107-crebots@users.noreply.github.com>
2020-08-18 13:36:01 -0500

    add stacker
    se ha agregado el control del stacker y se ha quitado el modulo de pwm de prueba

--- serto-stepper/serto-stepper.ino
+++ serto-stepper/serto-stepper.ino
@@ -13,50 +13,44 @@ using namespace arduino_due::pwm_lib;
 #define dir_izq 39
 #define pull_aux 7
 #define dir_aux 35
+#define INICIO_CARRERA  51
+#define FIN_CARRERA   53
+
+#define GO_HOME     1
+#define READY_TO_GO 2
+#define GO_FIN      3
+#define STOP_FIN    4
 
 AccelStepper motor_derecho(1,pull_der,dir_der);
 AccelStepper motor_izquierdo(1,pull_izq,dir_izq);
 AccelStepper motor_aux(1,pull_aux,dir_aux);
 
-
-uint32_t angles[ANGLES]= // degrees
-{
-  0,
-  45, 
-  90,
-  135,
-  180,
-  135,
-  90,
-  45
-};
-uint32_t angle=0;
-uint32_t last_angle;
-
-servo<pwm_pin::PWML0_PB16> servo_pwm_pinDAC1; // PB16 is DUE's pin DAC1
-
 capture_tc0_declaration();
-capture_tc8_declaration();
+//capture_tc8_declaration();
 capture_tc7_declaration();
 
 auto& capture_pin2=capture_tc0;
 auto& capture_pin3=capture_tc7;
-auto& capture_pin11=capture_tc8;
+//auto& capture_pin11=capture_tc8;
 
 uint32_t status2,duty2,period2,int1;
 uint32_t status3,duty3,period3,int2;
-uint32_t status4,duty4,period4,int3;
+//uint32_t status4,duty4,period4,int3;
 
 float der,izq,aux;
 long sup_izq, inf_izq, sup_der, inf_der, sup_aux, inf_aux;
+bool homes,fin;
+int estado_stacker=GO_HOME;
 
 void setup() {
   Serial.begin(250000);
 
-  pinMode(50,INPUT_PULLUP);
   capture_pin2.config(CAPTURE_TIME_WINDOW);
   capture_pin3.config(CAPTURE_TIME_WINDOW);
-  capture_pin11.config(CAPTURE_TIME_WINDOW);
+  pinMode(4,INPUT);
+  pinMode(INICIO_CARRERA,INPUT);
+  pinMode(FIN_CARRERA,INPUT);
+  //capture_pin11.config(CAPTURE_TIME_WINDOW);
 
   motor_derecho.setMaxSpeed(3000);
   motor_derecho.setSpeed(0);
@@ -67,24 +61,8 @@ void setup() {
   motor_izquierdo.setAcceleration(1);
   
   motor_aux.setMaxSpeed(3000);
-  motor_aux.setSpeed(0);
-  motor_aux.setAcceleration(1000);
-
-  
-  servo_pwm_pinDAC1.start(
-    PWM_PERIOD, // pwm servo period
-    100000, // 1e-8 secs. (1 msecs.), minimum duty value
-    200000, // 1e-8 secs. (2 msecs.), maximum duty value
-    0, // minimum angle, corresponding minimum servo angle
-    180, // maximum angle, corresponding minimum servo angle
-    angles[angle] // initial servo angle 
-  );
-  last_angle=millis();
-  Serial.println("********************************************************");
-  Serial.print("angle "); Serial.print(angle); 
-  Serial.print(": "); Serial.print(angles[angle]); 
-  Serial.println(" dgs.");
-  Serial.println("********************************************************");
+  motor_aux.setSpeed(200);
+  motor_aux.setAcceleration(100);
   
 }
 
@@ -94,10 +72,9 @@ void loop() {
   int1=duty2/capture_pin2.ticks_per_usec();
   status3=capture_pin3.get_duty_and_period(duty3,period3);
   int2=duty3/capture_pin3.ticks_per_usec();
-  status4=capture_pin11.get_duty_and_period(duty4,period4);
-  int3=duty4/capture_pin11.ticks_per_usec();
+  //status4=capture_pin11.get_duty_and_period(duty4,period4);
+  //int3=duty4/capture_pin11.ticks_per_usec();
 
-  
   if(1470>int1 && int1>950){
     der=-mapf(int1, 1470, 1050, 0, 2000);
     motor_derecho.setSpeed(der);
@@ -124,26 +101,10 @@ void loop() {
     izq=0;
   }
   
-  if(1450>int3 && int3>950){
-    aux=-mapf(int3, 1470, 1000, 0, 760);
-    motor_aux.setSpeed(aux);
-    motor_aux.runSpeed();
-  }else if(1550<int3 && int1<2100){
-    aux=mapf(int3, 1580, 2000, 0, 760);
-    motor_aux.setSpeed(aux);
-    motor_aux.runSpeed();
-  }else{
-    motor_aux.stop();
-    aux=0;
-  }
+  stacker();
   
 
-  if(millis()-last_angle>ANGLE_CHANGE_TIME)
-  {
-    angle=(angle+1)&0x07;
-    //servo_pwm_pinDAC1.set_angle(angles[angle]);
-    last_angle=millis();
-/*    
+  /*  
     Serial.print("entrada 1  "); 
     Serial.println(int1);
   
@@ -155,9 +116,45 @@ void loop() {
   
     Serial.print("angle ");
     Serial.print(angles[angle]); */
-  }
 }
 
 float mapf(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+static void stacker(){
+  homes=digitalRead(INICIO_CARRERA);
+  fin=digitalRead(FIN_CARRERA);
+  
+   switch (estado_stacker) {
+    
+    case GO_HOME:
+      if(!homes){
+        motor_aux.setSpeed(600);
+        motor_aux.runSpeed();
+      }else{
+        motor_aux.stop();
+        motor_aux.setCurrentPosition(0);
+        estado_stacker=READY_TO_GO;
+      }
+      break;
+      
+    case READY_TO_GO:  
+      if(digitalRead(4)){estado_stacker=GO_FIN;}
+      break;
+      
+    case GO_FIN:  
+      if(!fin){
+        motor_aux.moveTo(7400);
+        motor_aux.run();
+      }else{
+        motor_aux.stop();
+        estado_stacker=STOP_FIN;
+      }
+      break;
+      
+    case STOP_FIN:
+      if(!digitalRead(4)){estado_stacker=GO_HOME;} 
+      break;
+   }
 }
